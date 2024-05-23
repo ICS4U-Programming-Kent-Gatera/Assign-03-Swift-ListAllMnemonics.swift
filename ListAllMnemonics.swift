db05ef1f41215929722af5e40ca2511097dbf2c0
Kent Gatera <kent.gatera@stu.ocsb.ca>
2024-05-23 15:50:08 +0000

    Haaha

--- ListAllMnemonics.swift
+++ ListAllMnemonics.swift
@@ -5,13 +5,9 @@ import Foundation
 * Describe what your program does.
 
 *
-
 * @author  Kent Gatera
-
 * @version 1.0
-
 * @since   2024-May-14
-
 */
 // MnemonicsCombos class
 final class MnemonicsCombos {
@@ -34,32 +30,53 @@ final class MnemonicsCombos {
         lettersForDigitDictionary["9"] = "WXYZ"
         lettersForDigitDictionary["0"] = " "
 
-        do {
-            // Getting/reading the input file.
-            let inputString = try String(contentsOf: inputFile)
-            let mnemonicNums = inputString.components(separatedBy: .newlines)
-            var errorLineIter = 1
-            var outputString = ""
 
-            for mnemonic in mnemonicNums {
-                // Making sure the list is clear.
-                mnemonicsOutputList.removeAll()
-                // Create list of all Mnemonic results
-                let result = ListAllMnemonics(mnemonic, mnemonicsOutputList, lettersForDigitDictionary)
-                outputString += "\(mnemonic): \(result)\n"
-                errorLineIter += 1
+
+        // User choice for extra functionality.
+        print("\t\t\tListAllMnemonics Assign-03 Swift \n\tThis pro will list all of a number's possible mnemonics.\n \t\t\t\ti.e: 343")
+        print("\n\t\t Press 1 (console)\t Press 2 (Input file).")
+        let userChoice = readLine()
+        if (userChoice == "2") {
+            do {
+                // Getting/reading the input file.
+                let inputString = try String(contentsOf: inputFile)
+                let mnemonicNums = inputString.components(separatedBy: .newlines)
+                var errorLineIter = 1
+                var outputString = ""
+
+                for mnemonic in mnemonicNums {
+                    // Making sure the list is clear.
+                    mnemonicsOutputList.removeAll()
+                    // Create list of all Mnemonic results
+                    let result = ListAllMnemonics(mnemonic, mnemonicsOutputList, lettersForDigitDictionary)
+                    outputString += "\(mnemonic): \(result)\n"
+                    errorLineIter += 1
+                    
+                }
+                try outputString.write(to: outputFile, atomically: true, encoding: .utf8)
                 
+                print("Done.")
+            } catch {
+                print("Invalid input path!")
+            }
+        } else if (userChoice == "1") {
+            print("Enter the mnemonic: ", terminator: "")
+            let mnemonic = readLine()
+            // Making sure the list is clear.
+            mnemonicsOutputList.removeAll()
+            // Create list of all Mnemonic results
+            let result = ListAllMnemonics(mnemonic, mnemonicsOutputList, lettersForDigitDictionary)
+            outputString += "\(mnemonic): \(result)\n"
+            
             }
             try outputString.write(to: outputFile, atomically: true, encoding: .utf8)
             
             print("Done.")
-        } catch {
-            print("Invalid input path!")
         }
     }
 
     // Recursive method that gets the MnemonicsCombos of a number.
-    private static func ListAllMnemonics(_ someNumString: String, _ mnemonicsOutputList: [String], _ alphNumMap: [String: String]) -> [String] {
+    func ListAllMnemonics(_ someNumString: String, _ mnemonicsOutputList: [String], _ alphNumMap: [String: String]) -> [String] {
         if someNumString.isEmpty {
             // Returning an empty list to prevent errors.
             return [""]
@@ -83,7 +100,7 @@ final class MnemonicsCombos {
             return mnemonicsCombos
         }
     }
-}
+
 
 // Call the main method
-MnemonicsCombos.main()
+    MnemonicsCombos.main()
